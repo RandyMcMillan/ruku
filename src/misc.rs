25d70f4770212e4827ab5f7da6bce4d14819fb0b
Hasan <mr.k779@outlook.com>
2024-07-02 06:28:09 +0600

    Update misc.rs

--- src/misc.rs
+++ src/misc.rs
@@ -1,7 +1,7 @@
 pub fn get_image_with_version(image: &str, version: &Option<String>) -> String {
     let mut image_version = "latest";
     if let Some(v) = version {
-        image_version = &v;
+        image_version = v;
     }
     format!("{}:{}", image, image_version)
 }
