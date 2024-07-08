9f1c128f32ab2a0badd88294fd77ff08306fe2cb
Hasan <mr.k779@outlook.com>
2024-07-09 03:56:08 +0600

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -1,27 +1,24 @@
 use std::path::PathBuf;
-use crate::logger::Logger;
 
 pub struct Config {
     pub ruku_root: PathBuf,
+    pub ruku_binary: PathBuf,
     pub data_root: PathBuf,
     pub git_root: PathBuf,
     pub app_root: PathBuf,
 }
 
 impl Config {
-    pub fn new(log: Logger) -> Result<Self, Box<dyn std::error::Error>> {
+    pub fn new() -> Result<Self, Box<dyn std::error::Error>> {
         let home_dir = home::home_dir().ok_or("Could not determine home directory")?;
         let ruku_root = home_dir.join(".ruku");
 
-        let data_root = ruku_root.join("data");
-        let git_root = ruku_root.join("repos");
-        let app_root = home_dir.join("apps");
-
         Ok(Config {
-            ruku_root,
-            data_root,
-            git_root,
-            app_root,
+            ruku_root: home_dir.join(".ruku"),
+            ruku_binary: PathBuf::from("/usr/bin/ruku"),
+            data_root: ruku_root.join("data"),
+            git_root: ruku_root.join("repos"),
+            app_root: home_dir.join("apps"),
         })
     }
 }
