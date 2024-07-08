dddaed6fbf4b1afafc154b9e35bdd3c4c2a8fba9
Hasan <mr.k779@outlook.com>
2024-07-09 03:56:16 +0600

    Update misc.rs

--- src/misc.rs
+++ src/misc.rs
@@ -5,3 +5,12 @@ pub fn get_image_name_with_version(image: &str, version: &Option<String>) -> Str
     }
     format!("{}:{}", image, image_version)
 }
+
+pub fn sanitize_app_name(app: &str) -> String {
+    app.chars()
+        .filter(|&c| c.is_alphanumeric() || c == '.' || c == '_' || c == '-')
+        .collect::<String>()
+        .trim_start_matches('/')
+        .trim_end()
+        .to_string()
+}
